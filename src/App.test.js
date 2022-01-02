db19e61371e03af2a3d7cef61a369deeb4a520f8
Madhavi96 <madhavi.16@cse.mrt.ac.lk>
2022-01-02 18:28:39 +0530

    fix: test comment

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// app tests
+// app tests begin here
 test('renders learn react link', () => {
   render(<App />);
   const titleElement = screen.getByText(/ToDo App/i);
