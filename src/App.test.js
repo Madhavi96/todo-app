70e742d5d7314a74fdf93917260379de280b5861
Madhavi96 <madhavi.16@cse.mrt.ac.lk>
2022-01-02 18:27:11 +0530

    fix: test comment for pr hook check

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// app tests
 test('renders learn react link', () => {
   render(<App />);
   const titleElement = screen.getByText(/ToDo App/i);
