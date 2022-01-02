2efaae0f598d9ce60e93017efe8541a2b461fca1
Madhavi96 <madhavi.16@cse.mrt.ac.lk>
2022-01-02 18:35:17 +0530

    fix: test commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 // app tests begin here
 test('renders learn react link', () => {
+  // test app rendering
   render(<App />);
   const titleElement = screen.getByText(/ToDo App/i);
   expect(titleElement).toBeInTheDocument();
